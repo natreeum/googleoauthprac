3533347d37efefd58ad78f918d32832c43992d56
natreeum <projectkmscom@gmail.com>
2023-05-09 18:28:44 +0900

    fcommit

---
+++ app.js
@@ -0,0 +1,81 @@
+const { default: axios } = require('axios');
+const express = require('express');
+const url = require('url');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const app = express();
+const PORT = 8080;
+
+// express에서 정적 파일 사용하기
+// https://expressjs.com/ko/starter/static-files.html
+app.use(express.static('public'));
+
+// cors 설정
+app.use(cors());
+
+// NOTE process.env는 dotenv라이브러리 사용
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const AUTHORIZE_URI = 'https://accounts.google.com/o/oauth2/v2/auth';
+const REDIRECT_URL = 'http://localhost:8080/oauth2/redirect';
+const RESPONSE_TYPE = 'code';
+const SCOPE = 'openid%20profile%20email';
+const ACCESS_TYPE = 'offline';
+const OAUTH_URL = `${AUTHORIZE_URI}?client_id=${CLIENT_ID}&response_type=${RESPONSE_TYPE}&redirect_uri=${REDIRECT_URL}&scope=${SCOPE}&access_type=${ACCESS_TYPE}`;
+
+const getToken = async (code) => {
+  try {
+    const tokenApi = await axios.post(
+      `https://oauth2.googleapis.com/token?code=${code}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&redirect_uri=${REDIRECT_URL}&grant_type=authorization_code`
+    );
+    const accessToken = tokenApi.data.access_token;
+
+    return accessToken;
+  } catch (err) {
+    return err;
+  }
+};
+
+const getUserInfo = async (accessToken) => {
+  try {
+    const userInfoApi = await axios.get(
+      `https://www.googleapis.com/oauth2/v2/userinfo?alt=json`,
+      {
+        headers: {
+          authorization: `Bearer ${accessToken}`,
+        },
+      }
+    );
+    return userInfoApi;
+  } catch (err) {
+    return err;
+  }
+};
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/auth/google', (req, res) => {
+  res.send(OAUTH_URL);
+});
+
+app.get('/oauth2/redirect', async (req, res) => {
+  res.sendFile(__dirname + '/login.html');
+});
+
+app.get('/oauth2/accesstoken', async (req, res) => {
+  const code = req.query.code;
+  const access_token = await getToken(code);
+  return res.send(access_token);
+});
+
+app.get('/user/data', async (req, res) => {
+  const access_token = req.headers.authorization;
+});
+
+app.listen(PORT, () => {
+  console.log('server on! http://localhost:' + PORT);
+});
